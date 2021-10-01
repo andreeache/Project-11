1f6f13b741ab67f5fc0d2aca63de7fed03c5dc79
Andreea Chelaru <andreea.chelaru@yahoo.com>
2021-10-01 11:47:29 +0200

    styled about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 import about from "../assets/about.png";
-import "../styles/Dropdown.css"
+import "../styles/Dropdown.css";
 import "../styles/Banner.css";
 
 function About() {
@@ -8,11 +11,12 @@ function About() {
       <div className="banner">
         <img src={about} alt="banner background" className="banner-img" />
       </div>
-      About us
       <div className="dropdown">
-        <div className="dropdown-title">
-          
-          Reliability
+        <div className="dropdown-category">
+          <div className="dropdown-title">
+            Reliability
+            {<FontAwesomeIcon icon={faChevronDown} />}
+          </div>
           <div className="dropdown-content">
             The ads posted on Kasa guarantee total reliability of the place. The
             photos are consistent with the accommodations, and all information
@@ -20,26 +24,33 @@ function About() {
           </div>
         </div>
 
-        <div className="dropdown-title">
-          Respect
+        <div className="dropdown-category">
+          <div className="dropdown-title">
+            Respect
+            {<FontAwesomeIcon icon={faChevronDown} />}
+          </div>
           <div className="dropdown-content">
             Caring is one of the founding values of Kasa. Any discriminatory
             behavior or disruptive behavior in the neighborhood will result in
             exclusion from our platform.
           </div>
         </div>
-
-        <div className="dropdown-title">
-          Service
+        <div className="dropdown-category">
+          <div className="dropdown-title">
+            Service
+            {<FontAwesomeIcon icon={faChevronDown} />}
+          </div>
           <div className="dropdown-content">
             Our teams are at your disposal to provide you with the best
             experience. Please do not hesitate to contact us if you have any
             questions.
           </div>
         </div>
-
-        <div className="dropdown-title">
-          Safty
+        <div className="dropdown-category">
+          <div className="dropdown-title">
+            Safety
+            {<FontAwesomeIcon icon={faChevronDown} />}
+          </div>
           <div className="dropdown-content">
             Safety is Kasa's priority. Both for our guests and for travelers,
             each accommodation is compliant with the safety criteria established
