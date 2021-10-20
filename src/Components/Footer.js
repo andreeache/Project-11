969ba23394cdabf0740716eb57d4cf0609512a5f
Andreea Chelaru <andreeache@gmail.com>
2021-10-20 21:07:40 +0200

    fix apartment card

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer">
       <img src={logo2} alt="kasa" className="footer-logo" />
-      <div className="footer-text">@2020Kasa.All reights reserved</div>
+      <div className="footer-text">@2020Kasa.All rights reserved</div>
     </div>
   );
 }
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,26 +4,24 @@ import "../styles/Homepage.css";
 
 function Homepage(props) {
   return (
-    <div> <Banner/>
-    <div className="homepage-main">
-      {props.data.map((e) => (
-        <div key={e.id}
-          className="accommodation-card"
-          style={{
-            backgroundImage: "url(" + e.cover + ")",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          <Link to={location => `/apartment/${e.id}` }>
-          <h1 className="accommodation-title">
-            {e.title}</h1>
+    <div> <Banner />
+      <div className="homepage-main">
+        {props.data.map((e) => (
+          <Link to={location => `/apartment/${e.id}`} key={e.id}
+            className="accommodation-card"
+            style={{
+              backgroundImage: "url(" + e.cover + ")",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <h1 className="accommodation-title">
+              {e.title}</h1>
           </Link>
-        </div>
-      ))}
-    </div>
-    </div>
+        ))}
+      </div>
+    </div >
   );
 }
 
